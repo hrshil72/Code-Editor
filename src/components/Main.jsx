a99d4cf0129ea65c5c789365a1d6bb678eb7510a
[Harshil Karir] <[harshilkarir72@gmail.com]>
2024-02-18 23:23:05 +0530

    Result section Done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import EditorContainer from "./EditorContainer";
+import Result from "./Result";
 
 const Main = () => {
   return (
     <>
       <Header />
       <EditorContainer />
+      <Result />
     </>
   );
 };
