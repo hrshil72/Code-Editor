db953344d05f79bd9a9cf239c1216b92f8256310
[Harshil Karir] <[harshilkarir72@gmail.com]>
2024-02-18 22:56:06 +0530

    Editor done

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,5 @@
 import { Box, styled } from "@mui/material";
-import SettingsIcon from "@mui/icons-material/Settings";
+import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
 import { Controlled as ControlledEditor } from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
@@ -7,48 +7,72 @@ import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
-
-const Span = styled(Box)`
-  background-color: #ff3c41;
-  padding: 2px 0px;
-  place-content: center;
-  text-align: center;
-  border-radius: 5px;
-  height: 20px;
-  width: 20px;
-  margin: 5px;
-`;
+import { useState } from "react";
 
 const Heading = styled(Box)`
   display: flex;
   background-color: #1d1e22;
+  padding: 8px 10px;
+  border-top: 5px solid #34363e;
   align-items: center;
   text-transform: uppercase;
   font-family: Arial, Helvetica, sans-serif;
   color: #aaaebc;
-  font-weight: 600;
+  font-weight: 500;
 `;
 
 const Header = styled(Box)`
   display: flex;
+  align-items: center;
   color: white;
-  background-color: #1d1e22;
+  background-color: color;
   justify-content: space-between;
 `;
 
-const Container = styled(Box)``;
+const Container = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  padding: 10px;
+`;
 
-const Editor = () => {
+const Editor = ({ language, sign, color, value, onChange }) => {
+  let [close, setClose] = useState(true);
+
+  const handleChange = (editor, data, value) => {
+    onChange(value);
+  };
+
   return (
-    <Container>
+    <Container style={close ? null : { flexGrow: 0 }}>
       <Header>
         <Heading>
-          <Span component="span">/</Span>
-          HTML
+          <Box
+            style={{
+              backgroundColor: color,
+              padding: "2px 0px",
+              placeContent: "center",
+              textAlign: "center",
+              borderRadius: "5px",
+              height: "20px",
+              width: "20px",
+              margin: "5px",
+              color: "#1D1E22",
+              fontSize: "15px",
+            }}
+            component="span">
+            {sign}
+          </Box>
+          {language}
         </Heading>
-        <SettingsIcon />
+        <CloseFullscreenIcon
+          fontSize="small"
+          onClick={() => setClose((prev) => !prev)}
+        />
       </Header>
       <ControlledEditor
+        value={value}
+        onBeforeChange={handleChange}
         options={{
           theme: "material",
           lineNumbers: true,
--- src/components/EditorContainer.jsx
+++ src/components/EditorContainer.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import Editor from "./Editor";
 import { Box, styled } from "@mui/material";
+import { DataContext } from "../context/DataProvider";
+import { useContext } from "react";
 
 const Container = styled(Box)`
   background-color: #060606;
   display: flex;
   justify-content: space-evenly;
-  padding: 15px 10px;
+  padding: 0px 10px 15px 10px;
 `;
 
 const EditorContainer = () => {
+  const { html, setHtml, css, setCss, js, setJs } = useContext(DataContext);
+
   return (
     <Container>
-      <Editor />
-      <Editor />
-      <Editor />
+      <Editor
+        language="HTML"
+        sign="/"
+        color="#FF3C41"
+        value={html}
+        onChange={setHtml}
+      />
+      <Editor
+        language="CSS"
+        sign="*"
+        color="#0EBEFF"
+        value={css}
+        onChange={setCss}
+      />
+      <Editor
+        language="JS"
+        sign="()"
+        color="#FCD000"
+        value={js}
+        onChange={setJs}
+      />
     </Container>
   );
 };
